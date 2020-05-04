28bd0a13c81d6322de1fd5be2047cef64da2620b
Iman Anooshehpour <imananoosheh@users.noreply.github.com>
2020-05-03 22:43:28 -0400

    UI2 implementatin at 80%, header and footer with badges are done.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
-function xYZ (){
+function xYZ() {
     var replacement = '';
-    replacement += '<div>| This is the replacement of the copyright section |</div>';
+    replacement += '<div class="row text-center">' +
+        '<div class="col-lg-6 col-md-12 social-badge"><a href="https://www.linkedin.com/in/imananoosheh/"><img src="img/badges/LinkedIn_Logo_Modified.svg" alt="My GitHub Profile"></a></div>' +
+        '<div class="col-lg-6 col-md-12 social-badge"><a href="https://github.com/imananoosheh/"><img src="img/badges/GitHub_logo_2013_modified.svg" alt="My GitHub Profile"></a></div>' +
+        '<div class="col-12"><p>Copyright © 2019-2020 Iman Anooshehpour All Rights Reserved.</p></div>' +
+        '</div>';
     return replacement;
 }
 $('#contact').empty().html(xYZ());
@@ -9,10 +13,10 @@ function responsiveMainBody() {
     var mbc = $('#main-body');
     var footerHeight = $('FOOTER').height();
     var headerHeight = $('HEADER').height();
-    var mbcHeight = (window.innerHeight-100)-footerHeight-headerHeight-40;
+    var mbcHeight = window.innerHeight - footerHeight - headerHeight - 180;
     mbc.height(mbcHeight);
 }
 
-function startupFunctions(){
+function startupFunctions() {
     responsiveMainBody();
 }
