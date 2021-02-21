c0d89797277e31639c4ae111ceb977c20a1efffc
Iman Anooshehpour <imananoosheh@gmail.com>
2021-02-20 21:31:12 -0500

    update hilly's page

---
+++ HBD2U(^_^)/ramtindaghighi.com/postload.js
@@ -0,0 +1,31 @@
+var elements = document.querySelectorAll(".animation");
+
+anime({
+    targets: ".animation",
+    rotate: "1turn",
+    easing: "linear",
+    loop: true,
+    duration: 3000
+});
+
+let artwork_addrres_list = ["media/tri-1.png","media/Chaos Ball.png","media/tri-3.png","media/imun.png","media/tri-2.png","media/tri-4.png"]
+
+function change_address(){
+    let artworks = document.getElementsByClassName("artwork");
+    if($(".animation>img").attr("src")==artwork_addrres_list[0]){
+        $(".animation>img").attr("src", artwork_addrres_list[1])
+    }else if($(".animation>img").attr("src")==artwork_addrres_list[1]){
+        $(".animation>img").attr("src", artwork_addrres_list[2])
+    }else if($(".animation>img").attr("src")==artwork_addrres_list[2]){
+        $(".animation>img").attr("src", artwork_addrres_list[3])
+    }else if($(".animation>img").attr("src")==artwork_addrres_list[3]){
+        $(".animation>img").attr("src", artwork_addrres_list[4])
+    }else if($(".animation>img").attr("src")==artwork_addrres_list[4]){
+        $(".animation>img").attr("src", artwork_addrres_list[5])
+    }else if($(".animation>img").attr("src")==artwork_addrres_list[5]){
+        $(".animation>img").attr("src", artwork_addrres_list[0])
+    }
+}
+
+
+setInterval(change_address, 1500);
