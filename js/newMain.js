3393f25bbd5efaff549346e3a64ebbccee960331
Iman Anooshehpour <imananoosheh@gmail.com>
2022-07-26 14:17:41 -0400

    andjusted Navbar and small stuff

--- js/newMain.js
+++ js/newMain.js
@@ -2,6 +2,7 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 const navLink = document.querySelectorAll(".nav-link");
+const navLogo = document.querySelector(".nav-logo");
 
 hamburger.addEventListener("click", mobileMenu);
 navLink.forEach(n => n.addEventListener("click", closeMenu));
@@ -9,9 +10,11 @@ navLink.forEach(n => n.addEventListener("click", closeMenu));
 function mobileMenu() {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
+    navLogo.classList.toggle("active");
 }
 
 function closeMenu() {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
+    navLogo.classList.remove("active");
 }
